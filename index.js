48e709116a34464c71eaa65d95ac5848103e8caf
Elaya413 <elayavathiselvam@gmail.com>
2023-10-14 14:48:20 +0530

    changes

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    //origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    //origin: "http://localhost:3000",
     credentials: true,
   })
 );
